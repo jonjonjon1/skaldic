7e7056e79db3282f2a11313785c09a18ec9ebab8
jonjonjon1 <jonarnar99@gmail.com>
2026-05-14 17:56:38 +0200

    Cleaned doc2vec file
    Cleaner visualization, random seed for reproducibility

--- pytorch/doc2vec_visualization.py
+++ pytorch/doc2vec_visualization.py
@@ -9,20 +9,34 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 from load_data import load_data
+from datetime import datetime
+from typing import Literal
+import random
+import numpy as np
+import os
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
+def seed_everything(seed: int):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+
 def parse_args():
-    parser = argparse.ArgumentParser(description="Input files and, ratio of input used and merges performed")
+    parser = argparse.ArgumentParser()
     parser.add_argument("--data_file", type=str, default="norse_poems.csv", help="Data filename, all data is in \"..\\data\" directory")
-    parser.add_argument("--vocab_size", default=1000)
-    parser.add_argument("--doc2vec_epochs", default=50)
-    parser.add_argument("--doc2vec_features", default=32)
+    parser.add_argument("--vocab_size", type=int, default=1000)
+    parser.add_argument("--doc2vec_epochs", type=int, default=50)
+    parser.add_argument("--doc2vec_features", type=int, default=64)
     parser.add_argument("--doc2vec_window", default=5)
-    parser.add_argument("--overwrite_models", default=False)
-    parser.add_argument("--n_authors", default=5)
-    parser.add_argument("--volume_row_fraction", default=.1)
+    parser.add_argument("--n_authors", type=int, default=6)
+    parser.add_argument("--min_n_stanzas", type=int, default=12)
+    parser.add_argument("--volume_row_fraction", type=float, default=.03)
+    parser.add_argument("--dim_red", type=str, choices=["PCA", "tSNE"], default="tSNE")
+    parser.add_argument("--overwrite_models", default=True)
+    parser.add_argument("--save_fig", type=bool, default=True)
+    parser.add_argument("--seed", type=int, default=793)
     return parser.parse_args()
 
 def train_doc2vec(tokenized_list: list[list[str]],
@@ -34,8 +48,10 @@ def train_doc2vec(tokenized_list: list[list[str]],
         vector_size=args.doc2vec_features,
         window=args.doc2vec_window,
         min_count=1,
-        workers=4,
-        epochs=args.doc2vec_epochs
+        # Has to be 1 to guarantee deterministic runs
+        workers=1,
+        epochs=args.doc2vec_epochs,
+        seed=args.seed
     )
 
     doc2vec_model.build_vocab(tagged_data)
@@ -50,14 +66,22 @@ def train_doc2vec(tokenized_list: list[list[str]],
 def visualize_author_diff(df: pd.DataFrame,
                           vectorized_stanzas: np.ndarray,
                           n_authors: int,
-                          min_stanza_freq: int=15,
+                          min_stanza_freq: int,
+                          dim_red: Literal["PCA", "tSNE"],
+                          random_state: int,
                           plot_dir: Path|None=None) -> None:
 
-    pca = PCA(n_components=3)
-    results = pca.fit_transform(vectorized_stanzas)
-    
-    #tsne = TSNE(n_components=3, perplexity=20, learning_rate=10, max_iter=2000)
-    #results = tsne.fit_transform(vectorized_stanzas)
+    if dim_red == "PCA":
+        pca = PCA(n_components=3, random_state=random_state)
+        results = pca.fit_transform(vectorized_stanzas)
+
+    else:
+        tsne = TSNE(n_components=3,
+                    perplexity=20,
+                    learning_rate=10,
+                    max_iter=2000,
+                    random_state=random_state)
+        results = tsne.fit_transform(vectorized_stanzas)
 
     df = df[~df["author"].str.contains("Anonymous", na=False)]
 
@@ -68,23 +92,63 @@ def visualize_author_diff(df: pd.DataFrame,
     logger.info(f"Sampling {len(sampled_authors)} authors from total of {len(df["author"].unique())}")
     filtered_df = df[df["author"].isin(sampled_authors)]
 
-    print(df.columns.tolist())
-
     def return_if_lower(x: pd.DataFrame): return x.sample(n=min(len(x), min_stanza_freq))
     df = filtered_df.groupby("author").apply(return_if_lower, include_groups=False)
     df = df.reset_index()
-    print(df.columns.tolist())
 
-    labels = df["author"].unique()
+    if plot_dir:
+        timestamp = datetime.now().strftime("%m_%d_%H_%M")
+        dest_path = plot_dir / f"volumes_{dim_red}_{timestamp}"
+    else: dest_path = None
+
+    _make_plot(df["author"], results, dim_red, dest_path)
+
+def visualize_volume_diff(df: pd.DataFrame,
+                          vectorized_stanzas: np.ndarray,
+                          fraction: float,
+                          dim_red: Literal["PCA", "tSNE"],
+                          random_state: int,
+                          plot_dir: Path|None=None) -> None:
+
+    if dim_red == "PCA":
+        pca = PCA(n_components=3,  random_state=random_state)
+        results = pca.fit_transform(vectorized_stanzas)
+    else:
+        tsne = TSNE(n_components=3,
+                    perplexity=20,
+                    learning_rate=10,
+                    max_iter=2000,
+                    random_state=random_state)
+        results = tsne.fit_transform(vectorized_stanzas)
+
+    #chosen_volumes = df[df["volume"].isin([1, 5, 7])]
+    chosen_volumes = df
+    sampled_rows = chosen_volumes.sample(frac=fraction)
+    logger.info(f"Sampling {sampled_rows.shape[0]} rows from total of {df.shape[0]}")
+    df = sampled_rows
+
+    if plot_dir:
+        timestamp = datetime.now().strftime("%m%d_%H%M%S")
+        dest_path = plot_dir / f"volumes_{dim_red}_{timestamp}"
+    else: dest_path = None
+
+    _make_plot(df["volume"], results, dim_red, dest_path)
+
+def _make_plot(series: pd.Series,
+               results: np.ndarray,
+               dim_red: Literal["PCA", "tSNE"],
+               dest_path: Path) -> None:
+    
+    fig = plt.figure(figsize=(14, 10))
+    ax = fig.add_subplot(111, projection="3d")
+
+    labels = series.unique()
 
     colors = sns.color_palette("hls", len(labels))
     label_color_map = dict(zip(labels, colors))
 
-    fig = plt.figure(figsize=(14, 10))
-    ax = fig.add_subplot(111, projection="3d")
-
-    for label in labels:
-        indices = [i for i, l in enumerate(df["author"]) if l == label]
+    for label in sorted(labels):
+        indices = [i for i, l in enumerate(series) if l == label]
         
         ax.scatter(
             results[indices,0],
@@ -97,21 +161,22 @@ def visualize_author_diff(df: pd.DataFrame,
             edgecolors="w"
         )
 
-    ax.set_title("PCA embeddings of documents (stanzas)", fontsize=15)
+    ax.set_title(f"{dim_red} embeddings of documents (stanzas)", fontsize=15)
     ax.legend(title="Labels", bbox_to_anchor=(1.05, 1), loc='upper left')
-    ax.set_xlabel('PC1')
-    ax.set_ylabel('PC2')
-    ax.set_zlabel('PC3')
+    ax.set_xlabel(f"{dim_red}1")
+    ax.set_ylabel(f"{dim_red}2")
+    ax.set_zlabel(f"{dim_red}3")
 
     plt.tight_layout()
-    plt.savefig(f"{plot_dir}/author_pca_3D.png", dpi=300, bbox_inches="tight")
-    plt.show()
-    plt.close()
+    if dest_path:
+        plt.savefig(f"{dest_path}_3D.png")
+    else:
+        plt.show()
 
     fig, ax = plt.subplots(figsize=(9, 8))
 
-    for label in labels:
-        indices = [i for i, l in enumerate(df["author"]) if l == label]
+    for label in sorted(labels):
+        indices = [i for i, l in enumerate(series) if l == label]
         
         ax.scatter(
             results[indices,0],
@@ -124,64 +189,19 @@ def visualize_author_diff(df: pd.DataFrame,
         )
 
     ax.grid(alpha=.5)
-    ax.set_title("PCA embeddings of documents (stanzas)", fontsize=15)
+    ax.set_title(f"{dim_red} embeddings of documents (stanzas)", fontsize=15)
     ax.legend(title="Labels")
-    ax.set_xlabel("PC1")
-    ax.set_ylabel("PC2")
+    ax.set_xlabel(f"{dim_red}1")
+    ax.set_ylabel(f"{dim_red}2")
 
-    plt.savefig(f"{plot_dir}/author_pca_2D.png")
-    plt.show()
-    plt.close()
-
-def visualize_volume_diff(df: pd.DataFrame,
-                          vectorized_stanzas: np.ndarray,
-                          fraction: float,
-                          plot_dir: Path|None=None) -> None:
-
-    pca = PCA(n_components=3)
-    pca_results = pca.fit_transform(vectorized_stanzas)
-    #tsne = TSNE(n_components=3, perplexity=20, learning_rate=10, max_iter=2000)
-    #tsne_results = tsne.fit_transform(vectorized_stanzas)
-
-    chosen_volumes = df[df["volume"].isin([1, 5, 7])]
-    sampled_rows = chosen_volumes.sample(frac=fraction)
-    logger.info(f"Sampling {sampled_rows.shape[0]} rows from total of {df.shape[0]}")
-    df = sampled_rows
-
-    fig = plt.figure(figsize=(14, 10))
-    ax = fig.add_subplot(111, projection="3d")
-
-    labels = df["volume"].unique()
-
-    colors = sns.color_palette("hls", len(labels))
-    label_color_map = dict(zip(labels, colors))
-
-    for label in sorted(labels):
-        indices = [i for i, l in enumerate(df["volume"]) if l == label]
-        
-        ax.scatter(
-            pca_results[indices,0],
-            pca_results[indices,1],
-            pca_results[indices,2],
-            label=label,
-            color=label_color_map[label],
-            s=60,
-            alpha=.7,
-            edgecolors="w"
-        )
-
-    ax.set_title("PCA embeddings of documents (stanzas)", fontsize=15)
-    ax.legend(title="Labels", bbox_to_anchor=(1.05, 1), loc="upper left")
-    ax.set_xlabel("PC1")
-    ax.set_ylabel("PC2")
-    ax.set_zlabel("PC3")
-
-    plt.tight_layout()
-    plt.savefig(f"{plot_dir}/volume_pca.png", dpi=300, bbox_inches="tight")
-    plt.show()
+    if dest_path:
+        plt.savefig(f"{dest_path}_2D.png")
+    else:
+        plt.show()
 
 if __name__=="__main__":
     args = parse_args()
+    seed_everything(args.seed)
     current_dir = Path(__file__).parent
 
     df = load_data(args)
@@ -189,7 +209,7 @@ if __name__=="__main__":
     bin_dir = current_dir / "bin"
     bin_dir.mkdir(parents=True, exist_ok=True)
 
-    doc2vec_default = bin_dir / "doc2vec"
+    doc2vec_default = bin_dir / "doc2vec.model"
     if not args.overwrite_models and next(bin_dir.glob("doc2vec*"), None):
         doc2vec_model = Doc2Vec.load(str(doc2vec_default))
     else:
@@ -198,8 +218,23 @@ if __name__=="__main__":
     tokenized = df["tokenized"]
     stanza_vectors = np.array([doc2vec_model.dv[i] for i in range(len(tokenized))])
 
-    plot_dir = current_dir / "plots"
-    plot_dir.mkdir(parents=True, exist_ok=True)
+    if args.save_fig:
+        plot_dir = current_dir / "plots"
+        plot_dir.mkdir(parents=True, exist_ok=True)
+    else:
+        plot_dir = None
 
-    visualize_author_diff(df, stanza_vectors, args.n_authors, plot_dir=plot_dir)
-    #visualize_volume_diff(df, stanza_vectors, args.volume_row_fraction, plot_dir=plot_dir)
+    visualize_author_diff(df,
+                          stanza_vectors,
+                          args.n_authors,
+                          args.min_n_stanzas,
+                          args.dim_red,
+                          args.seed,
+                          plot_dir=plot_dir)
+    
+    visualize_volume_diff(df,
+                          stanza_vectors,
+                          args.volume_row_fraction,
+                          args.dim_red,
+                          args.seed,
+                          plot_dir=plot_dir)
